ffc9098fe35793bedf21e2707ca6200fadc72075
Benson Lee <bensonlee5@gmail.com>
2022-01-22 21:24:30 -0800

    upd

--- layout/navs/NavigationDark.tsx
+++ layout/navs/NavigationDark.tsx
@@ -63,7 +63,7 @@ export default function NavigationDark() {
                       href={item.href}
                       className={classNames(
                         item.current
-                          ? "bg-gray-900 text-white"
+                          ? "bg-gray-900 text-white px-3 py-2 rounded-md text-lg font-medium"
                           : "text-white hover:bg-gray-700 hover:text-gray-300 px-3 py-2 rounded-md text-lg font-medium"
                       )}
                       aria-current={item.current ? "page" : undefined}
@@ -149,7 +149,7 @@ export default function NavigationDark() {
                   href={item.href}
                   className={classNames(
                     item.current
-                      ? "bg-gray-900 text-white"
+                      ? "bg-gray-900 text-white px-3 py-2 rounded-md text-base font-medium"
                       : "text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                   )}
                   aria-current={item.current ? "page" : undefined}
