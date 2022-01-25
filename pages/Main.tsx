509d0ad60ba754f8f805f9cd9f5337df2e4c91cd
Benson Lee <bensonlee5@gmail.com>
2022-01-25 08:17:12 -0800

    upd

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -97,12 +97,7 @@ export default function Main() {
                 <div className="flex items-center justify-between w-full md:w-auto">
                   <a href="/">
                     <span className="sr-only">Workflow</span>
-                    <Image
-                      src="/logo/ganymede_icon_circle.png"
-                      alt=""
-                      width={35}
-                      height={35}
-                    />
+                    <Image src="/logo/ganymede_icon_circle.png" alt="" />
                   </a>
                   <div className="-mr-2 flex items-center md:hidden">
                     <Popover.Button className="bg-gray-50 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
