d20d4e56602504d3afb68ab49c172b183d4086b5
Benson Lee <bensonlee5@gmail.com>
2022-07-21 17:46:36 -0700

    more cleanup

--- components/backgrounds/MoleculeStarsBackground.tsx
+++ components/backgrounds/MoleculeStarsBackground.tsx
@@ -3,7 +3,7 @@ import { imageHostURL } from "../../utils/constants/constants";
 export default function MoleculeStarsBackground(props: any) {
   return (<div className="bg-repeat"
     style={{
-      backgroundImage: `url('/molecule_stars.png')`,
+      backgroundImage: `url('${imageHostURL}/molecule_stars.png')`,
       backgroundSize: '500px'
     }}>
     {props.children}
