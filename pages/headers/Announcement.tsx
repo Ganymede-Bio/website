085200ea61101e50d4e3cf95c302e75306f5bdc5
Benson Lee <bensonlee5@gmail.com>
2022-12-21 21:21:49 -0800

    upd

--- pages/headers/Announcement.tsx
+++ pages/headers/Announcement.tsx
@@ -8,7 +8,7 @@ export default function Announcement() {
   return (
     <>
       {!showBanner &&
-        (<div className="relative bg-regal-purple">
+        (<div className="relative bg-regal-purple invisible md:visible">
           <div className="mx-auto max-w-7xl py-2 px-3 sm:px-6 lg:px-8">
             <div className="text-center">
               <p className="font-medium text-white">
@@ -22,7 +22,7 @@ export default function Announcement() {
             <div className="absolute inset-y-0 right-0 flex items-start pt-1 pr-1 sm:items-start sm:pt-1 sm:pr-2">
               <button
                 type="button"
-                className="flex rounded-md p-1 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white invisible md:visible"
+                className="flex rounded-md p-1 hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white"
               >
                 <span className="sr-only">Dismiss</span>
                 <XMarkIcon className="h-6 w-6 text-white" aria-hidden="true" onClick={() => setShowBanner(!showBanner)} />
--- pages/headers/Header.tsx
+++ pages/headers/Header.tsx
@@ -3,7 +3,7 @@ import Navbar from "../../components/menus/NavBar"
 
 export default function Header() {
   return (
-    <header className="top-0 left-0 w-full md:sticky z-10 absolute backdrop-filter backdrop-blur mt-10 md:mt-0">
+    <header className="top-0 left-0 w-full md:sticky z-10 absolute backdrop-filter backdrop-blur">
       <Navbar />
     </header >
   )
